154e7e951cc14c076fd930e07cc6f38cdb49887a
DilekaHapugoda <dilekadhanushi@gmail.com>
2019-08-06 16:37:55 +0530

    commet 1

--- app/src/main/java/com/example/shoppingapp/RegisterActivity.java
+++ app/src/main/java/com/example/shoppingapp/RegisterActivity.java
@@ -2,13 +2,78 @@ package com.example.shoppingapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.sql.DatabaseMetaData;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private Button CreateAccountButton;
+    private EditText InputName, InputPhoneNumber, InputPassword;
+    private ProgressDialog loadingBar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        CreateAccountButton = (Button) findViewById(R.id.register_btn);
+        InputName = (EditText) findViewById(R.id.register_username_input);
+        InputPassword = (EditText) findViewById(R.id.register_password_input);
+        InputPhoneNumber = (EditText) findViewById(R.id.register_number);
+        loadingBar = new ProgressDialog(this);
+
+
+        CreateAccountButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                CreateAccount();
+
+            }
+        });
+
     }
+
+
+    private void CreateAccount(){
+        String name = InputName.getText().toString();
+        String phone = InputPhoneNumber.getText().toString();
+        String password = InputPassword.getText().toString();
+
+        if(TextUtils.isEmpty(name)){
+
+            Toast.makeText(this, "Please write your name...", Toast.LENGTH_SHORT).show();
+        }
+
+        else if(TextUtils.isEmpty(phone)){
+
+            Toast.makeText(this, "Please write your Phone Number...", Toast.LENGTH_SHORT).show();
+        }
+
+        else if(TextUtils.isEmpty(password)){
+
+            Toast.makeText(this, "Please enter your password...", Toast.LENGTH_SHORT).show();
+        }
+
+        else{
+            loadingBar.setTitle("Create Account");
+            loadingBar.setMessage("Please wait, While we are checking the credentials.");
+            loadingBar.setCanceledOnTouchOutside(false);
+            loadingBar.show();
+
+
+
+        }
+
+    }
+
+
+
 }
--- app/src/main/java/com/example/shoppingapp/login.java
+++ app/src/main/java/com/example/shoppingapp/login.java
@@ -3,8 +3,14 @@ package com.example.shoppingapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class login extends AppCompatActivity {
+
+    private EditText InputNumber, InputPassword;
+    private Button LoginButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
